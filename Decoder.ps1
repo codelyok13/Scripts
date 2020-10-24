d7ce7288632be97095ad82173bfb0abaedffea5a
Kendricks <codelyok13@live.com>
2020-10-23 23:16:29 -0400

    First

---
+++ Decoder.ps1
@@ -0,0 +1,14 @@
+﻿$input = Read-Host "Enter String to convert"
+$shift = Read-Host "Enter the shfit value"
+
+$charArray = $input.ToCharArray()
+
+$output = ""
+$shift = $shift%37
+
+foreach ($char in $charArray)
+{
+    $output += ([char]([int]$char-$shift))
+}
+
+Write-Host $output
---
+++ DisplaySwitch.ps1
@@ -0,0 +1,10 @@
+﻿#while($true){echo $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")}
+$KeyPress = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+optionValue = $KeyPress.VirtualKeyCode -band 3
+switch(optionValue)
+{
+    1{displayswitch.exe /internal} #Display 1 is the only screen active
+    2{displayswitch.exe /clone} #Clone Display 1 to every connected monitor
+    3{DisplaySwitch.exe /extend} #All Displays are active
+    4{DisplaySwitch.exe /external} #show only on a projector
+}
